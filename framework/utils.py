9896aebe956fb1d2fcd0c6f35b3468493b11f635
Saurav Kumar <saurav.kumar90@hotmailcom>
2020-02-09 20:32:30 +0530

    Added new tests

--- framework/utils.py
+++ framework/utils.py
@@ -1,8 +1,13 @@
 import os
+import pandas as pd
 from dotenv import load_dotenv
 
 
 def load_env_configs(file_name=".env"):
-    #print("loading contents of .env")
     env_path = os.getcwd() + '/../{}'.format(file_name)
     load_dotenv(verbose=True, dotenv_path=env_path)
+
+def replace_null(df):
+    df.replace({None: pd.np.nan}, inplace=True)
+
+    return df
--- test/test_compare.py
+++ test/test_compare.py
@@ -1,15 +1,16 @@
 # External packages
 import sqlite3
-import pymysql
 import pandas as pd
+import os
 from sqlalchemy import create_engine
 
 # Internal packages
 from framework.test_base import TestBase
+from framework.utils import replace_null
 
 class TestCompare(TestBase):
 
-    def test_login_successful(self):
+    def test_fetch_from_dbl(self):
         """ Test to compare to datasets """
         self.assertTrue(2>1)
 
@@ -28,3 +29,38 @@ class TestCompare(TestBase):
         pd.set_option('display.expand_frame_repr', False)
 
         print(frame)
+
+
+    def test_fetch_large_data(self):
+        sqlEngine = create_engine("mysql://root:root@localhost/demo", pool_recycle=3600)
+        db_connection = sqlEngine.connect()
+
+        # Step 1: fetched records from target table into df_target using chunk_size
+        chunk_size = 100000
+        sql_query = "select * from demo.employees"
+        df_list = []
+
+        for chunk in pd.read_sql(sql_query, db_connection, chunksize=chunk_size):
+            df_list.append(chunk)
+
+        df_target = pd.concat(df_list, ignore_index=True)
+        print(len(df_target))
+
+        # Step 2: read source data from csv
+        test_file_path = os.getcwd() + "/../test_data/employees.csv"
+        df_source = pd.read_csv(test_file_path)
+        print(len(df_source))
+
+        # Step 3: Pre-processing
+        df_target = replace_null(df_target)
+        df_source = replace_null(df_source)
+
+        # Assertion
+        self.assertEqual(len(df_source), len(df_target), "Record length mismatch b/w source and target...")
+
+        headers_in_source = len(df_source.columns.values)
+        headers_in_target = len(df_target.columns.values)
+        self.assertTrue(headers_in_source == headers_in_target)
+
+        # Step 4: Comparison
+        # TODO : assignment
